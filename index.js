5f345497d782294f109c1669069a272887e2bd88
Adriana Pitt <adrianapitt2001@gmail.com>
2019-05-23 22:24:09 +0100

    Update index.js
    Added channel specific command and removed innocent from general.

--- index.js
+++ index.js
@@ -19,15 +19,19 @@ client.on('message', (msg) => {
   if (msg.content === '!>isthebestcommand') {
     msg.channel.send('Adriana knows she fucked up not using > as the prefix.')
   }
-  if (msg.content === '!innocent') {
-    msg.channel.send('https://cdn.discordapp.com/attachments/569217733498109956/576378343985905664/Screenshot_20190509-084144_Discord.jpg')
-  }
   if (msg.content === '!hoe') {
     msg.channel.send('  https://cdn.discordapp.com/attachments/529740146338299905/578616783016165387/Screenshot_20190516-091624_Discord.jpg')
   }
-  if (msg.content === !purpose') {
+  if (msg.content === '!purpose') {
     msg.channel.send('At this moment in time I have no purpose apart from helping Ana learn JS.')
   }
+  client.on('message', msg => {
+    if (msg.channel.id === '571483749095833600') {
+        if (msg.content === '!purpose') {
+          msg.channel.send('https://cdn.discordapp.com/attachments/569217733498109956/576378343985905664/Screenshot_20190509-084144_Discord.jpg')
+    }
+});
+
 });
 
 client.on('ready', ()=>{
