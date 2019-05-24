4c4d9df232cd5bdaf20159ffb922c8b3a32f5d79
SiegeOneTapper <48494803+SiegeOneTapper@users.noreply.github.com>
2019-05-24 12:07:11 +0100

    Update index.js
    Up to date with Master Branch

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const token = 'NTgxMDkyMDk4NTAyMDMzNDA4.XOaOag.ha0qg86SZA1CGPrU1RF8EMX40qY';
 
 const client = new Discord.Client();
 client.on('message', (msg) => {
+  if (msg.channel.id === '571483769962496001') {
+    if (msg.content === '!innocent')
+      msg.channel.send('https://cdn.discordapp.com/attachments/569217733498109956/576378343985905664/Screenshot_20190509-084144_Discord.jpg')
+  }
   if (msg.content === '!test'){
     msg.channel.send(`Hello ${msg.author}!`);
   }
@@ -13,21 +17,26 @@ client.on('message', (msg) => {
   if (msg.content === '!bed') {
     msg.channel.send('Get out of bed you lovely person!')
   }
+  if (msg.content === '!sleep') {
+    msg.channel.send('https://www.youtube.com/watch?v=Udj-o2m39NA')
+  }
   if (msg.content === '!>isthebestcommand') {
     msg.channel.send('Adriana knows she fucked up not using > as the prefix.')
   }
-  if (msg.content === ' Innocent') {
-    msg.channel.send('https://cdn.discordapp.com/attachments/569217733498109956/576378343985905664/Screenshot_20190509-084144_Discord.jpg')
+  if (msg.content === '!hoe') {
+    msg.channel.send('https://cdn.discordapp.com/attachments/529740146338299905/578616783016165387/Screenshot_20190516-091624_Discord.jpg')
   }
-  if (msg.content === 'Hoe') {
-    msg.channel.send('  https://cdn.discordapp.com/attachments/529740146338299905/578616783016165387/Screenshot_20190516-091624_Discord.jpg')
+  if (msg.content === '!purpose') {
+    msg.channel.send('At this moment in time I have no purpose apart from helping Ana learn JS.')
   }
 });
+
+
 
 client.on('ready', ()=>{
   console.log('Bot is now connected');
 
-  //client.channels.find(x => x.name === 'general-chattings').send('Hello! I\'m now connected!');
+  client.channels.find(x => x.name === 'general-chattings').send('SkyNet is now online. Executing extermination subroutine..');
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
